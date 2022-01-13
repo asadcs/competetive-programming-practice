8f811cd6df096bdf902587f823c8fbb474194f7b
Muhammad Asadullah <muhammad.asad2020@gmail.com>
2022-01-14 01:52:19 +0500

    Reverse string three types

--- Competitive programming/Strings/Strings-practice/Program.cs
+++ Competitive programming/Strings/Strings-practice/Program.cs
@@ -43,8 +43,8 @@ namespace Strings_practice
             
         }
 
-        //Program No : 4 Reverse a String 
-        public static void ReverseAString() {
+        //Program No : 4(a) Reverse Characters in String 
+        public static void ReverseCharInString() {
             //Step :01 string is given
             string message = "Hello World";
             //Step 02: convert string into Char Array
@@ -62,6 +62,73 @@ namespace Strings_practice
             {
                 Console.Write(B[i]);
             }
+
+
+        }
+
+        //Program No : 4(b) Reverse Words in String 
+        public static void ReverseWordsInString()
+        {
+            //Youtube Ref : https://www.youtube.com/watch?v=M-XYuPCTfgY
+            //Step :01 string is given
+            string message = ".I.LIKE...THIS.PROGRAM";
+            string container = string.Empty;
+           
+
+            foreach(var substring in message.Split('.'))
+            {
+                container = substring + "." + container;
+;           }
+
+            container = container.Remove(container.Length - 1);
+
+            Console.Write(container);
+            
+            //Algo:
+            //Convert target string into char array ;
+            //Declare an empty container string to hold the reverse order words
+            //Idea is starting from end , one by one eleminating a word from string S1
+            //using substring function . it requires two params 1. start index , length of the  
+            //start a loop from the end to start
+            //Define a variable that count the traversed chars till it finds a "."
+
+            //Calculate the length of the targe array 
+            //
+
+
+
+        }
+
+        //Program No : 4(C) Reverse Each Word in String 
+        public static void ReverseEachWordsInString()
+        {
+            //Youtube Ref : https://www.youtube.com/watch?v=M-XYuPCTfgY
+            //Step :01 string is given
+            string message = ".I.LIKE...THIS.PROGRAM";
+            string container = string.Empty;
+
+
+            foreach (var substring in message.Split('.'))
+            {
+                container = substring + "." + container;
+                ;
+            }
+
+            container = container.Remove(container.Length - 1);
+
+            Console.Write(container);
+
+            //Algo:
+            //Convert target string into char array ;
+            //Declare an empty container string to hold the reverse order words
+            //Idea is starting from end , one by one eleminating a word from string S1
+            //using substring function . it requires two params 1. start index , length of the  
+            //start a loop from the end to start
+            //Define a variable that count the traversed chars till it finds a "."
+
+            //Calculate the length of the targe array 
+            //
+
 
 
         }
@@ -130,6 +197,41 @@ namespace Strings_practice
 
             Console.Write(IsPalindrome);
         }
+
+        //Program No : 7  Convert Lower To Upper
+        public static void ConvertLowerToUpper()
+        {
+            string slogan= "pakistan zindabad";
+
+            char[] A = slogan.ToCharArray();
+
+            for(int i=0;i<slogan.Length;i++)
+            {
+                A[i] = Convert.ToChar(Convert.ToInt32(A[i]) - 32);
+                Console.Write(A[i]);
+            }
+
+
+        }
+        //Program No : 8  Convert Lower To Upper
+        public static void ConvertFirstLetterLowerToUpper()
+        {
+            string slogan = "pakistan zindabad is the moto of life";
+
+            char[] A = slogan.ToCharArray();
+
+            for (int i = 0; i < slogan.Length; i++)
+            {
+                if(A[i]==' ')
+                {
+                    A[i+1] = Convert.ToChar(Convert.ToInt32(A[i+1]) - 32);
+                   
+                }
+                Console.Write(A[i]);
+            }
+
+
+        }
         static void Main(string[] args)
         {
             //Program No : 1 Hello World 
@@ -141,14 +243,23 @@ namespace Strings_practice
             //Program No : 3 Copy One String to Another String
             // CopyOneStringToAnotherString();
 
-            //Program No : 4 Reverse a String 
-            // ReverseAString();
+            //Program No : 4(a) Reverse Characters in String 
+            //ReverseCharInString();
+
+            //Program No : 4(b) Reverse Words in String 
+            // ReverseWordsInString();
 
             //Program No : 5 CheckPalindrome 
             // CheckPalindrome();
 
             //Program No : 6 CheckPalindrome 
-            CheckPalindromeWithSpaces();
+            // CheckPalindromeWithSpaces();
+
+            //Program No : 7  Convert Lower To Upper
+            //ConvertLowerToUpper();
+
+            //Program No : 8  Convert Lower To Upper
+            ConvertFirstLetterLowerToUpper();
 
             Console.ReadLine();
         }
